a368859b0a98473fc89d4e9fdee65d276af2ff15
josianed <jsu.dufour@gmail.com>
2023-12-14 22:21:00 +0000

    Day 14 part 1 additional comments

--- day14/day14-solution.js
+++ day14/day14-solution.js
@@ -58,26 +58,33 @@ transposedRows.forEach((transposedRow) => {
       // count empty spaces
       emptySpaces += 1;
     } else if (item === "#") {
+      // once hit a square rock, construct row with all round rocks
+      // at the beginning, and all empty spaces at the end
       for (let j = 0; j < roundRocks; j++) {
         newRow += "O";
       }
       for (let k = 0; k < emptySpaces; k++) {
         newRow += ".";
       }
+      // append square rock found in current iteration
       newRow += "#";
+      // reset counts
       roundRocks = 0;
       emptySpaces = 0;
     }
   });
   // at end of a row with no square rocks
   for (let j = 0; j < roundRocks; j++) {
+    // construct row with all round rocks
     newRow += "O";
   }
   for (let k = 0; k < emptySpaces; k++) {
+    // construct row with all empty spaces
     newRow += ".";
   }
   roundRocks = 0;
   emptySpaces = 0;
+  // row is shifted, append to new array
   shiftedRows.push(newRow);
 });
 
@@ -97,8 +104,10 @@ const totalRows = transposedColumns.length;
 transposedColumns.forEach((column, index) => {
   let numberSquareRocks = 0;
   column.split("").forEach((item) => {
+    // count number of square rocks per row
     if (item === "O") numberSquareRocks += 1;
   });
+  // load represents total rows - current row index
   const rowLoad = numberSquareRocks * (totalRows - index);
   sum += rowLoad;
 });
